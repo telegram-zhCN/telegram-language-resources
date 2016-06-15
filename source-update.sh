d6d5e723cb95744221fdf14fa7582c521fe923fb
mikelei8291 <mikelei8291@gmail.com>
2016-06-15 18:54:11 +0800

    Update file fetching script

--- source-update.sh
+++ source-update.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-cookie="X-Mapping-fjhppofk=;ajs_group_id=;csrftoken=;sessionid="
+cookie=""
 dir=source
 
 #Desktop
+echo "Desktop"
 curl -b $cookie https://www.transifex.com/telegram/telegram-desktop/langstrings/en/download/for_use/ -o $dir/lang-desktop.strings --progress
 #Android
+echo "Android"
 curl -b $cookie https://www.transifex.com/telegram/telegram/stringsxml-48/en/download/for_use/ -o $dir/strings-android.xml --progress
 #iOS
+echo "iOS"
 curl -b $cookie https://www.transifex.com/telegram/iphone-1/localizablestrings/en/download/for_use/ -o $dir/Localizable-iOS.strings --progress
 #OSX
+echo "OS X"
 curl -b $cookie https://www.transifex.com/telegram/osx/localizablestrings/en/download/for_use/ -o $dir/Localizable-OSX.strings --progress
 #Webogram
+echo "Webogram"
 curl -b $cookie https://www.transifex.com/telegram/telegram-web/en-usjson/en/download/for_use/ -o $dir/strings-web.json --progress
 #WP
+echo "WP"
 curl -b $cookie https://www.transifex.com/telegram/wp-telegram-messenger-beta/appresourcesresx/en/download/for_use/ -o $dir/AppResources-WP.resx --progress
--- translate-update.sh
+++ translate-update.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-cookie="X-Mapping-fjhppofk=;ajs_group_id=;csrftoken=;sessionid="
+cookie=""
 dir=zh-CN
 
 #Desktop
+echo "Desktop"
 curl -b $cookie https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/lang-desktopstrings/zh_CN/download/for_use/ -o $dir/lang-desktop.strings --progress
 #Android
+echo "Android"
 curl -b $cookie https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/tgcn-android/zh_CN/download/for_use/ -o $dir/strings-android.xml --progress
 #iOS
+echo "iOS"
 curl -b $cookie https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/tgcn-ios/zh_CN/download/for_use/ -o $dir/Localizable-iOS.strings --progress
 #OSX
+echo "OS X"
 curl -b $cookie https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/localizable-osxstrings/zh_CN/download/for_use/ -o $dir/Localizable-OSX.strings --progress
 #Webogram
+echo "Webogram"
 curl -b $cookie https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/strings-webjson/zh_CN/download/for_use/ -o $dir/strings-web.json --progress
 #WP
+echo "WP"
 curl -b $cookie https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/appresources-wpresx/zh_CN/download/for_use/ -o $dir/AppResources-WP.resx --progress
