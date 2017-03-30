da90cbcc35f8fe81b529a50a12fc84c7919c0cef
mikelei8291 <mikelei8291@gmail.com>
2017-03-30 08:45:30 +0800

    Update 20170330 - Add main menu translation for macOS client - Fix bug in webogram translation - Add macOS translation package generating code in translation fetching   script

--- translate-update.sh
+++ translate-update.sh
@@ -15,9 +15,18 @@ curl -b $cookie https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/te
 #macOS
 echo "macOS"
 curl -b $cookie https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/telegram-macos/zh_CN/download/for_use/ -o $dir/Localizable-macOS.strings --progress
+echo "macOS MainMenu"
+curl -b $cookie https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/telegram-macos-mainmenu/zh_CN/download/for_use/ -o $dir/Localizable-MainMenu-macOS.strings --progress
 #Webogram
 echo "Webogram"
 curl -b $cookie https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/telegram-webogram/zh_CN/download/for_use/ -o $dir/strings-web.json --progress
 #WP
 echo "WP"
 curl -b $cookie https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/telegram-wp/zh_CN/download/for_use/ -o $dir/AppResources-WP.resx --progress
+#Generate zip file for macOS
+cd $dir/
+mkdir -p Contents/Resources/zh.lproj
+cp Localizable-macOS.strings Contents/Resources/zh.lproj/Localizable.strings
+cp Localizable-MainMenu-macOS.strings Contents/Resources/zh.lproj/MainMenu.strings
+zip -r9 Localizable-macOS.zip Contents
+rm -rf Contents
