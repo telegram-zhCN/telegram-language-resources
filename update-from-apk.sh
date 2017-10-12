363ad6b2623e1667e96d6f6dda5140caaa038325
wfjsw <wfjsw@users.noreply.github.com>
2017-10-12 23:32:53 +0800

    fix ubuntu no apktool

--- update-from-apk.sh
+++ update-from-apk.sh
@@ -1,7 +1,8 @@
 mkdir temp
 cd temp
+wget -O apktool.jar "https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.3.0.jar"
 wget -O telegram.apk "https://rink.hockeyapp.net/api/2/apps/dc3b3c6317af4a3caa5269a58697e088?format=apk"
-apktool d telegram.apk
+java -jar apktool.jar d telegram.apk
 cd telegram/res/values
 cp strings.xml ../../../../source/strings-android.xml
 cd ../../../../
