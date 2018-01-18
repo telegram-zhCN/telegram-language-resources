f621cedfe9859a9538c7a814ff8d1d9bf5098871
brainbush <h@qaq.gd>
2018-01-18 17:34:25 +0800

    Update python script

--- source-update.py
+++ source-update.py
@@ -5,11 +5,17 @@ from bs4 import BeautifulSoup
 
 identification = os.environ.get('TRANSIFEX_USERNAME')
 password = os.environ.get("TRANSIFEX_PASSWORD")
+stel_ssid = os.environ.get('STEL_SSID')
+stel_token = os.environ.get('STEL_TOKEN')
 
 if identification is None:
     identification = sys.argv[1]
 if password is None:
     password = sys.argv[2]
+if stel_ssid is None:
+    stel_ssid = sys.argv[3]
+if stel_token is None:
+    stel_token = sys.argv[4]
 
 directory = 'source/'
 
@@ -21,26 +27,6 @@ csrftoken = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
 post = {'identification': identification, 'password': password, 'csrfmiddlewaretoken': csrftoken}
 r = s.post("https://www.transifex.com/signin/", data=post, headers={'referer': 'https://www.transifex.com/signin/'})
 
-print('Desktop')
-r = s.get('https://www.transifex.com/telegram/telegram-desktop/langstrings/en/download/for_use/')
-with open(directory + "lang-desktop.strings", "wb") as code:
-    code.write(r.content)
-
-print('Android')
-r = s.get('https://www.transifex.com/telegram/telegram/stringsxml-48/en/download/for_use/')
-with open(directory + "strings-android.xml", "wb") as code:
-    code.write(r.content)
-
-print('iOS')
-r = s.get('https://www.transifex.com/telegram/iphone-1/localizablestrings/en/download/for_use/')
-with open(directory + "Localizable-iOS.strings", "wb") as code:
-    code.write(r.content)
-
-print('macOS')
-r = s.get('https://www.transifex.com/telegram/osx/swiftapplocalizablestrings/en/download/for_use/')
-with open(directory + "Localizable-macOS.strings", "wb") as code:
-    code.write(r.content)
-
 print('Webogram')
 r = s.get('https://www.transifex.com/telegram/telegram-web/en-usjson/en/download/for_use/')
 with open(directory + "strings-web.json", "wb") as code:
@@ -49,4 +35,26 @@ with open(directory + "strings-web.json", "wb") as code:
 print('wp')
 r = s.get('https://www.transifex.com/telegram/wp-telegram-messenger-beta/appresourcesresx/en/download/for_use/')
 with open(directory + "AppResources-WP.resx", "wb") as code:
+    code.write(r.content)
+
+print('Fetching sources from telegram.org')
+cookies = {'stel_ssid': stel_ssid, 'stel_token': stel_token}
+print('Desktop')
+r = requests.get('https://translations.telegram.org/en/tdesktop/export', cookies=cookies)
+with open(directory + "lang-desktop.strings", "wb") as code:
+    code.write(r.content)
+
+print('Android')
+r = requests.get('https://translations.telegram.org/en/android/export', cookies=cookies)
+with open(directory + "strings-android.xml", "wb") as code:
+    code.write(r.content)
+
+print('iOS')
+r = requests.get('https://translations.telegram.org/en/ios/export', cookies=cookies)
+with open(directory + "Localizable-iOS.strings", "wb") as code:
+    code.write(r.content)
+
+print('macOS')
+r = requests.get('https://translations.telegram.org/en/macos/export', cookies=cookies)
+with open(directory + "Localizable-macOS.strings", "wb") as code:
     code.write(r.content)
