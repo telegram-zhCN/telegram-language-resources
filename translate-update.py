67beef4e7c8a2b28eb1bff7c406100d13f16e6ec
Jabasukuriputo Wang <wfjsw@users.noreply.github.com>
2018-12-14 00:38:00 +0800

    Update translate-update.py

--- translate-update.py
+++ translate-update.py
@@ -5,11 +5,17 @@ from bs4 import BeautifulSoup
 
 identification = os.environ.get('TRANSIFEX_USERNAME')
 password = os.environ.get("TRANSIFEX_PASSWORD")
+stel_ssid = os.environ.get('STEL_SSID')
+stel_token = os.environ.get('STEL_TOKEN')
 
 if identification is None:
     identification = sys.argv[1]
 if password is None:
     password = sys.argv[2]
+if stel_ssid is None:
+    stel_ssid = sys.argv[3]
+if stel_token is None:
+    stel_token = sys.argv[4]
 
 directory = 'zh-CN/'
 
@@ -21,31 +27,6 @@ csrftoken = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
 post = {'identification': identification, 'password': password, 'csrfmiddlewaretoken': csrftoken}
 r = s.post("https://www.transifex.com/signin/", data=post, headers={'referer': 'https://www.transifex.com/signin/'})
 
-print('Desktop')
-r = s.get('https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/telegram-desktop/zh_CN/download/for_use/')
-with open(directory + "lang-desktop.strings", "wb") as code:
-    code.write(r.content)
-
-print('Android')
-r = s.get('https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/telegram-android/zh_CN/download/for_use/')
-with open(directory + "strings-android.xml", "wb") as code:
-    code.write(r.content)
-    
-print('Android X')
-r = s.get('https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/telegram-androidx/zh_CN/download/for_use/')
-with open(directory + "strings-androidx.xml", "wb") as code:
-    code.write(r.content)
-
-print('iOS')
-r = s.get('https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/telegram-ios/zh_CN/download/for_use/')
-with open(directory + "Localizable-iOS.strings", "wb") as code:
-    code.write(r.content)
-
-print('macOS')
-r = s.get('https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/telegram-macos/zh_CN/download/for_use/')
-with open(directory + "Localizable-macOS.strings", "wb") as code:
-    code.write(r.content)
-
 print('Webogram')
 r = s.get('https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/telegram-webogram/zh_CN/download/for_use/')
 with open(directory + "strings-web.json", "wb") as code:
@@ -54,4 +35,31 @@ with open(directory + "strings-web.json", "wb") as code:
 print('wp')
 r = s.get('https://www.transifex.com/telegram-zhcn/telegram-zhcn-allplat/telegram-wp/zh_CN/download/for_use/')
 with open(directory + "AppResources-WP.resx", "wb") as code:
+    code.write(r.content)
+
+print('Fetching sources from telegram.org')
+cookies = {'stel_ssid': stel_ssid, 'stel_token': stel_token}
+print('Desktop')
+r = requests.get('https://translations.telegram.org/classic-zh-cn/tdesktop/export', cookies=cookies)
+with open(directory + "lang-desktop.strings", "wb") as code:
+    code.write(r.content)
+
+print('Android')
+r = requests.get('https://translations.telegram.org/classic-zh-cn/android/export', cookies=cookies)
+with open(directory + "strings-android.xml", "wb") as code:
+    code.write(r.content)
+
+print('iOS')
+r = requests.get('https://translations.telegram.org/classic-zh-cn/ios/export', cookies=cookies)
+with open(directory + "Localizable-iOS.strings", "wb") as code:
+    code.write(r.content)
+
+print('macOS')
+r = requests.get('https://translations.telegram.org/classic-zh-cn/macos/export', cookies=cookies)
+with open(directory + "Localizable-macOS.strings", "wb") as code:
+    code.write(r.content)
+
+print('Android X')
+r = requests.get('https://translations.telegram.org/classic-zh-cn/android_x/export', cookies=cookies)
+with open(directory + "strings-androidx.xml", "wb") as code:
     code.write(r.content)
